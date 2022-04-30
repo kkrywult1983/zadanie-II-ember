cf9ba4b04175fc054aaf65b278e1ffc568f48ecb
Kamil Krywult <cutmaster12@gmail.com>
2022-04-30 14:12:18 +0200

    Add each while to serwer user datas.

--- app/users/route.js
+++ app/users/route.js
@@ -1,3 +1,27 @@
 import Route from '@ember/routing/route';
 
-export default class UsersRoute extends Route {}
+export default class UsersRoute extends Route {
+  model() {
+    const users = [
+      {
+        id: 1,
+        username: 'admin',
+        password: 'admin123',
+        email: 'admin@admin.com',
+        isDeleted: false,
+        isAdmin: true,
+      },
+
+      {
+        id: 2,
+        username: 'user',
+        password: 'user123',
+        email: 'user@user.com',
+        isDeleted: false,
+        isAdmin: false,
+      },
+    ];
+
+    return users;
+  }
+}
