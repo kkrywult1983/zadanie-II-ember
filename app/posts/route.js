df093d11ca6a475c30b05b560f3a0219d67486ca
Kamil Krywult <cutmaster12@gmail.com>
2022-04-30 15:42:17 +0200

    Added posts to post.

--- app/posts/route.js
+++ app/posts/route.js
@@ -1,3 +1,39 @@
 import Route from '@ember/routing/route';
 
-export default class PostsRoute extends Route {}
+export default class PostsRoute extends Route {
+  model() {
+    const posts = [
+      {
+        id: 1,
+        ownerId: 1,
+        title: 'Tytuł testowy 1',
+        body: 'Zawartość testowa 1',
+        likesCount: 0,
+        isDeleted: false,
+        likedByUserIds: [],
+        dislikedByUserIds: [],
+      },
+      {
+        id: 2,
+        ownerId: 1,
+        title: 'Tytuł testowy 2',
+        body: 'Zawartość testowa 2',
+        likesCount: 0,
+        isDeleted: false,
+        likedByUserIds: [],
+        dislikedByUserIds: [],
+      },
+      {
+        id: 3,
+        ownerId: 1,
+        title: 'Tytuł testowy 3',
+        body: 'Zawartość testowa 3',
+        likesCount: 0,
+        isDeleted: false,
+        likedByUserIds: [],
+        dislikedByUserIds: [],
+      },
+    ];
+    return posts;
+  }
+}
