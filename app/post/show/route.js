9f8ba4b34eff005f20520fe1752d72cb4e47cc38
Kamil Krywult <cutmaster12@gmail.com>
2022-05-07 15:13:38 +0200

    Add link to posts- details.

--- app/post/show/route.js
+++ app/post/show/route.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class PostShowRoute extends Route {}
+export default class PostShowRoute extends Route {
+  @service store;
+  async model(params) {
+    const posts = this.store.findRecord('post', params.id);
+    return posts;
+  }
+}
--- app/router.js
+++ app/router.js
@@ -10,7 +10,7 @@ Router.map(function () {
   this.route('posts');
 
   this.route('post', function () {
-    this.route('show');
+    this.route('show', { path: '/:id' });
   });
   this.route('users', function () {});
 
