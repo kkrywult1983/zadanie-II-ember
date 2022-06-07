0beb56c9ff37e4942448a59a9c32707f7527e808
Kamil Krywult <cutmaster12@gmail.com>
2022-06-07 22:08:42 +0200

    Test

--- app/components/posts/list/item/component.js
+++ app/components/posts/list/item/component.js
@@ -1,7 +1,18 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
-export default class PostLikeButton extends Component {
-  mojaZmienna = 'Kamil';
+export default class PostsListItemComponent extends Component {
+  // mojaZmienna = 'Kamil';
+  @service session;
+
+  get isAllreadyLikedByCurrentUser() {
+    const { currentUser } = this.session;
+    const { post } = this.args;
+
+    const LikeModel = currentUser.likes.find((like) => {
+      return like.post.get('id') === post.id;
+    });
+    const isLikeExists = Boolean(LikeModel);
+    return isLikeExists;
+  }
 }
-
-// Komponent do zliczania lików
